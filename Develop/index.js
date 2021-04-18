cecef752f112100323838832cba69f09ad7f62b2
cliffybar <crowellclifford@utexas.edu>
2021-04-18 15:26:49 -0500

    copied questions from first assignment attempt

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Project Title",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "What is the project description?"
+    },
+    {
+        type: "input",
+        name: "Installation Instructions",
+        message: "How is this project installed?"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "How do you use this app?"
+    },
+    {
+        type: "input",
+        name: "Contributions",
+        message: "Please list all contributors to this project."
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Please provide tests for this project"
+    },
+    {
+        type: "input",
+        name: "Username",
+        message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "GitHub",
+        message: "What is your GitHub repo link?"
+    }
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +52,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
